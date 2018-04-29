a0526501d7af222433dff7b2a6f4b29525b89b00
superxc <superxcgm@gmail.com>
2018-04-29 16:00:10 +0800

    update GameFrame game logic

--- src/main/java/com/superxc/chineseIdioms/view/GameFrame.java
+++ src/main/java/com/superxc/chineseIdioms/view/GameFrame.java
@@ -1,25 +1,29 @@
 package com.superxc.chineseIdioms.view;
 
+import com.sun.tools.javac.util.Pair;
 import com.superxc.chineseIdioms.model.Idiom;
 import com.superxc.chineseIdioms.model.User;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameFrame extends CenterableFrame{
 
     private static final String PROMPT_OFF = "提示：关";
     private static final int COLS = 7;
-    private final JPanel centerPanel;
-    private final JPanel southPanel;
+
     private Map<Integer, Idiom> idioms;
+
     private boolean prompt = false;
+
     private int currentPromptIndex = -1;
+
+    private List<JButton> wordsClick = new ArrayList<>();
+    private JLabel labelPrompt;
 
     public GameFrame(ChoseStageFrame choseStageFrame, User user, int stage) {
 
@@ -32,8 +36,8 @@ public class GameFrame extends CenterableFrame{
 
         setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
 
-        centerPanel = new JPanel();
-        southPanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+        JPanel southPanel = new JPanel();
 
         initIdioms(stage);
 
@@ -59,11 +63,11 @@ public class GameFrame extends CenterableFrame{
     private void initializeSouthPanel(JPanel southPanel) {
         southPanel.setLayout(new BorderLayout());
 
-        JLabel labelPrompt = new JLabel(PROMPT_OFF, JLabel.CENTER);
+        labelPrompt = new JLabel(PROMPT_OFF, JLabel.CENTER);
         labelPrompt.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
-                refreshPrompt(labelPrompt);
+                refreshPrompt();
             }
         });
         southPanel.add(labelPrompt, BorderLayout.CENTER);
@@ -72,7 +76,7 @@ public class GameFrame extends CenterableFrame{
         buttonPrompt.addActionListener(e -> {
             prompt = !prompt;
             if (prompt) {
-                refreshPrompt(labelPrompt);
+                refreshPrompt();
             } else {
                 labelPrompt.setText(PROMPT_OFF);
             }
@@ -80,7 +84,7 @@ public class GameFrame extends CenterableFrame{
         southPanel.add(buttonPrompt, BorderLayout.SOUTH);
     }
 
-    private void refreshPrompt(JLabel labelPrompt) {
+    private void refreshPrompt() {
         if (!prompt) {
             return;
         }
@@ -97,16 +101,62 @@ public class GameFrame extends CenterableFrame{
     private void initializeCenterPanel(JPanel centerPanel) {
         int rows = getRows();
         centerPanel.setLayout(new GridLayout(rows, COLS));
+        int index = 0;
         for (Idiom idiom : idioms.values()) {
             String[] words = idiom.getSplit();
             for (String word : words) {
                 JButton button = new JButton(word);
-                button.addActionListener(e -> {
-                    System.out.println("click : " + e.getActionCommand());
-                });
+                index++;
+                button.addActionListener(handleWordClick());
                 centerPanel.add(button);
             }
         }
+    }
+
+    private ActionListener handleWordClick() {
+        return e -> {
+            JButton button = (JButton) e.getSource();
+            if (wordsClick.contains(button)) {
+                setButtonActive(button, false);
+                wordsClick.remove(button);
+                return;
+            }
+
+            wordsClick.add(button);
+            setButtonActive(button, true);
+            if (wordsClick.size() != 4) {
+                return;
+            }
+
+            String sIdiom = String.join("", wordsClick.stream().map(btn -> btn.getActionCommand()).collect(Collectors.toList()));
+            Integer index;
+            if ((index = guessRight(sIdiom)) != null) {
+                if (idioms.get(index).getDescription().equals(labelPrompt.getText())) {
+                    refreshPrompt();
+                }
+                wordsClick.forEach(btn -> btn.setVisible(false));
+                idioms.remove(index);
+            }
+            wordsClick.forEach(btn -> {
+                setButtonActive(btn, false);
+            });
+            wordsClick.clear();
+        };
+    }
+
+    private void setButtonActive(JButton button, boolean active) {
+        button.setBackground(active ? Color.green : getBackground());
+        button.setOpaque(active);
+        button.setBorderPainted(!active);
+    }
+
+    private Integer guessRight(String sIdiom) {
+        for (Integer integer : idioms.keySet()) {
+            if (sIdiom.equals(idioms.get(integer).getValue())) {
+                return integer;
+            }
+        }
+        return null;
     }
 
     private int getRows() {
